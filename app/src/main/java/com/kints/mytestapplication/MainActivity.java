5b7f019f5863a1a8e9ed008d46c8982c0c49726c
100RAB <saurabhupankar@yahoo.com>
2019-07-31 12:32:13 +0530

    Simple listview created

--- app/src/main/java/com/kints/mytestapplication/MainActivity.java
+++ app/src/main/java/com/kints/mytestapplication/MainActivity.java
@@ -2,12 +2,20 @@ package com.kints.mytestapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+ ListView listview;
+ String[]mobile={"Appple","Blackberry","Nokia","Samsung","Redmi","Oppo","Vivo","Realme"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listview=findViewById(R.id.mobile_list);
+
+        ArrayAdapter adapter=new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,mobile);
+        listview.setAdapter(adapter);
     }
 }
